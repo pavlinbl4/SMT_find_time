03bf730b0d754e4c04c0e8041d2042b512fe70e7
evgeniy <pavlinl4@gmail.com>
2023-02-21 18:25:12 +0300

    now I want select days

--- find_free_time/cabinet.py
+++ find_free_time/cabinet.py
@@ -3,17 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
-
 from must_have.crome_options import setting_chrome_options
 from must_have.credentials import get_credentials
-
-
-import time
+from must_have.soup import get_soup
 
 
 def enter_cabinet():
-
     login, password = get_credentials()
     browser = webdriver.Chrome(options=setting_chrome_options())
     browser.get('https://clinic-complex.ru/cabinet/')
@@ -36,33 +31,33 @@ def enter_cabinet():
     WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "appointment-online__form")))
 
-
     Select(WebDriverWait(browser, 10).until(
         EC.element_to_be_clickable((By.ID, "POPUP_SPECIALIZATION")))).select_by_value("29889")
 
     Select(WebDriverWait(browser, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR,
-                                    "body > main > section > form > fieldset.appointment-online__form-fieldset.--col-2.step4 > label > div > select")))).select_by_value("4000392")
+                                    "body > main > section > form > "
+                                    "fieldset.appointment-online__form-fieldset.--col-2.step4 >"
+                                    " label > div > select")))) \
+        .select_by_value("4001454")  # 4001454 - Болдырева   4000392 - Адферов
 
+    page_html = browser.page_source
+    return page_html
+    # print(browser.find_element(By.CLASS_NAME, 'active day'))
 
-    print(browser.find_element(By.CLASS_NAME, 'active day'))
+    # time.sleep(10)
 
-
-
-
-
-
-
-    time.sleep(10)
-
-    browser.close()
-    browser.quit()
-
-
-
-
-
+    # browser.close()
+    # browser.quit()
 
 
 if __name__ == '__main__':
-    enter_cabinet()
+    soup = get_soup(enter_cabinet())
+    month = soup.find('div', class_='datepicker-days').find(class_="datepicker-switch").text
+    active_day = soup.find('div', class_='datepicker-days').find_all('tr')
+    # print(f'ближайщий день  {active_day.text} {month}')
+    # other_days = soup.find('div',class_='datepicker-days').find('tbody').find_all(class_="day")
+    print(soup.find(class_="fio").text)
+    print(month)
+    for i in active_day:
+        print(i)
