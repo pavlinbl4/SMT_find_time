23b9a1de1060c76d7af3bcda049ad5cdc681b93f
evgeniy <pavlinbl4@gmail.com>
2023-02-23 23:23:32 +0300

    good it works for manual doctor and specialisation code modification

--- find_free_time/cabinet.py
+++ find_free_time/cabinet.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import Select
 from must_have.crome_options import setting_chrome_options
 from must_have.credentials import get_credentials
 from must_have.soup import get_soup
+import time
 
 
 def select_specealization(specialization_index):
@@ -14,12 +15,43 @@ def select_specealization(specialization_index):
 
 
 def select_doctor(doctor_index):
-    Select(WebDriverWait(browser, 40).until(
+    doctor = Select(WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR,
                                     "body > main > section > form > "
                                     "fieldset.appointment-online__form-fieldset.--col-2.step4 >"
-                                    " label > div > select")))) \
-        .select_by_value(doctor_index)
+                                    " label > div > select"))))
+    time.sleep(5)
+    doctor.select_by_value(doctor_index)
+
+
+def get_information_from_page(page_html):  # receive information from page fo activ day for selected doctor
+    soup = get_soup(page_html)
+    month = soup.find('div', class_='datepicker-days').find(class_="datepicker-switch").text  # current month
+    active_day = soup.find('div', class_='datepicker-days').find(class_="active day").text  # activ (selected) day
+    all_days = soup.find('div', class_='datepicker-days').find_all(class_="day")  # all days
+    doctor_family_name = soup.find(class_="fio").text  # doctor family
+    address = soup.find(class_='data__item data__item__address').text  # clinick adress
+    active_day_time = soup.find(class_="spanSelectors").find_all('span', class_="time")  # active day appointments
+    return month, active_day, active_day_time, address, doctor_family_name, all_days
+
+
+def select_day_from_list(day_number):
+    day = browser.find_element(By.CLASS_NAME, 'table-condensed').find_element(By.TAG_NAME, 'tbody') \
+        .find_element(By.XPATH, f"//td[(text()={day_number}) and not(contains(@class, 'disabled'))]")
+    day.is_selected()
+    WebDriverWait(browser, 20).until(EC.element_to_be_clickable(day))
+    day.click()
+
+
+def appointment_date_and_time(active_day_time, all_days):
+    time_in_active_day_list = [x.text for x in active_day_time]
+    aviable_days_list = [i.text for i in all_days if 'disabled' not in i.get('class')]
+    return aviable_days_list, time_in_active_day_list
+
+
+def select_clinic():
+    browser.find_element(By.XPATH, '/html/body/main/section/form/fieldset[9]/label[2]/span').click()  # select clinic
+    return browser.page_source
 
 
 def enter_cabinet():
@@ -44,42 +76,36 @@ def enter_cabinet():
     WebDriverWait(browser, 20).until(
         EC.presence_of_element_located((By.CLASS_NAME, "appointment-online__form")))
 
-    select_specealization("29889")
-
-    select_doctor("4001454")  # 4001454 - Болдырева   4000392 - Адферов
-
-    browser.find_element(By.XPATH, '/html/body/main/section/form/fieldset[9]/label[2]/span').click()
-
-    page_html = browser.page_source
-    return page_html
-
-    browser.close()
-    browser.quit()
-
 
 if __name__ == '__main__':
     browser = webdriver.Chrome(options=setting_chrome_options())
-    soup = get_soup(enter_cabinet())
-    month = soup.find('div', class_='datepicker-days').find(class_="datepicker-switch").text
-    trs = soup.find('div', class_='datepicker-days').find('tbody').find_all('tr')
-    active_day = soup.find('div', class_='datepicker-days').find(class_="active day").text
-    other_days = soup.find('div', class_='datepicker-days').find_all(class_="new day")
-    all_days = soup.find('div', class_='datepicker-days').find_all(class_="day")
+    enter_cabinet()  # authorisation only
+    # select_specealization("29889")  # select specialisation  дерматология
+    select_specealization("6106")  # select specialisation  гастроэнтология
+    # select_doctor("4001454")
+    # select_doctor("4001728")  # Марченко
+    select_doctor("4000957")  # Гаглоева
+    # select_doctor("4000392")  # Алферов
+    page_html = select_clinic()
 
-    doctor_family_name = soup.find(class_="fio").text
+    month, active_day, active_day_time, address, doctor_family_name, all_days = get_information_from_page(page_html)
 
-    address = soup.find(class_='data__item data__item__address').text
+    appointment_dict = {}
 
-    active_day_time = soup.find(class_="spanSelectors").find_all('span', class_="time")
+    aviable_days_list, time_in_active_day_list = appointment_date_and_time(active_day_time, all_days)
+    appointment_dict[active_day] = time_in_active_day_list
+
+    for i in range(1, len(aviable_days_list)):
+        select_day_from_list(aviable_days_list[i])
+        page_html = browser.page_source
+        month, active_day, active_day_time, address, doctor_family_name, all_days = get_information_from_page(page_html)
+        aviable_days_list, time_in_active_day_list = appointment_date_and_time(active_day_time, all_days)
+        appointment_dict[active_day] = time_in_active_day_list
 
     print(doctor_family_name)
     print(address)
     print(month)
-
-    time_in_active_day_list = [x.text for x in active_day_time]
-
-    aviable_days_list = [i.text for i in all_days if 'disabled' not in i.get('class')]
-    print(f'{aviable_days_list = }')
-
-    print(f"{active_day = }")
-    print(f"{time_in_active_day_list = }")
+    for i in appointment_dict:
+        print(i, appointment_dict[i])
+    browser.close()
+    browser.quit()
