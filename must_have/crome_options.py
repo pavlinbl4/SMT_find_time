88f1c0d0feac263e3b4a2f0bdf777cd46d548635
evgeniy <pavlinbl4@gmail.com>
2023-02-23 23:24:19 +0300

    added opton to fullscreen browser

--- must_have/crome_options.py
+++ must_have/crome_options.py
@@ -7,6 +7,7 @@ def setting_chrome_options():
     chrome_options = Options()
     chrome_options.add_experimental_option('detach', True)
     # chrome_options.add_argument("--headless")  # фоновый режим
+    chrome_options.add_argument("--start-maximized") #
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # невидимость автоматизации
     chrome_options.add_argument(
         "user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
